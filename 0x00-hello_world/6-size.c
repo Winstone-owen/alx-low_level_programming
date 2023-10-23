a2d8361d4d25b36e709dbe4281c223a23c91bbbb
Winstone-owen <maywakaowen@gmail.com>
2023-10-23 12:00:40 +0300

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,25 +1,15 @@
 #include <stdio.h>
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*
-* FUNCTIONALITY *
-*
-* 1. We are printing the size of a char using the sizeof() operator.
-* 2. We are printing the size of an int using the sizeof() operator.
-* 3. We are printing the size of a long int using the sizeof() operator.
-* 4. We are printing the size of a long long int using the sizeof() operator.
-* 5. We are printing the size of a float using the sizeof() operator.
-*
-* Time Complexity: O(1)
-*/
-int main(void) 
+ * main - print the size of various types
+ * Description: prints the size of various types on the computer it is compiled
+ * and run on
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-printf("Size of a char: %i byte(s)\n", sizeof(char));
-printf("Size of an int: %i byte(s)\n", sizeof(int));
-printf("Size of a long int: %i byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
-printf("Size of a float: %i byte(s)\n", sizeof(float));
+printf("Size of a char: %d byte(s)\n", (int) sizeof(char));
+printf("Size of an int: %d byte(s)\n", (int) sizeof(int));
+printf("Size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
+printf("Size of a float: %d byte(s)\n", (int) sizeof(float));
 return (0);
 }
