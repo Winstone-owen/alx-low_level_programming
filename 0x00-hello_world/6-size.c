02d2945af44544c9d90a56b7d5e47e9ac032eb1b
Winstone-owen <maywakaowen@gmail.com>
2023-10-23 12:16:47 +0300

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,17 +3,21 @@
 /**
  * main - print the size of various types
  *
- * Description: prints the size of various types on the computer it is compiled
- * and run on
+ * Description: This program prints the size of various data types
+ * to the standard output. It is intended to run on both 32-bit
+ * and 64-bit systems.
  *
- * Return: Always 0 (Success)
+ * Return: 0 (SUCCESS)
  */
-
 int main(void)
 {
-printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof(char));
-printf("Size of an int: %lu byte(s)\n", (unsigned long) sizeof(int));
-printf("Size of a long long int: %lu byte(s)\n", (unsigned long) sizeof(long long int));
-printf("Size of a float: %lu byte(s)\n", (unsigned long) sizeof(float));
-return 0;
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+printf("Size of an int: %lu byte(s)\n", sizeof(int));
+printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %lu byte(s)\n", sizeof(float));
+printf("Size of a double: %lu byte(s)\n", sizeof(double));
+
+return (0);
 }
+
