65cddeabf383b93384d775149ba22fb69e700e1c
Winstone-owen <maywakaowen@gmail.com>
2023-10-23 20:08:50 +0300

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,32 +1,17 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
-* main - main function in the task
+* main - print the size of various types
 *
+* Description: prints the size of various types on the computer it is compiled
+* and run on
 *
-* return: return 1
-*
-* FUNCTIONALITY *
-*
-* The first line of the code is a comment.
-* Comments are ignored by the compiler.
-*
-* The next line includes the standard input/output library.
-* This library contains functions such as printf, scanf, etc.
-*
-* The next line is the main function. Every C program must have a main function.
-*
-* The next line is a function call to printf.
-* printf is a function that prints a string to the standard output.
-*
-* The next line is a return statement. Every function in C must return a value.
-*
-* The last line is the end of the main function.
-*
-* Time Complexity: O(1)
+* Return: Always 0 (Success)
 */
-int main(void)
+
+
+int main()
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
+char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(STDERR_FILENO, msg, 42);
+return 1;
 }
