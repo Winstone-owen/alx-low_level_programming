ee711cb5feb36eb60032816434dbdf6920e15b04
Winstone-owen <maywakaowen@gmail.com>
2023-10-23 20:01:06 +0300

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,8 +2,14 @@
 
 /**
  * main - print the size of various types
- * Return: 0
+ *
+ * Description: This program prints the size of various data types
+ * to the standard output. It is intended to run on both 32-bit
+ * and 64-bit systems.
+ *
+ * Return: 0 (SUCCESS)
  */
+
 int main(void)
 {
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
