2aa138f5522002e43e5bb9340fc26f54e8e097bf
Winstone-owen <maywakaowen@gmail.com>
2023-10-24 20:50:10 +0300

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,33 +1,13 @@
-#include "main.h"
+#include <string.h>
 
 /**
- * _strstr - locates a substring
- *
- * @haystack: the longer string to search
- * @needle: the first occurrence of the substring
- *
- * Return: a pointer beg of substring or @Null if it not found.
- */
+* *_strstr - Finds first occurence of substring
+* @haystack: String to be scanned
+* @needle: String to search for
+* Return: Pointer to haystack that matches needle
+*/
+
 char *_strstr(char *haystack, char *needle)
 {
-unsigned int i = 0, j = 0;
-
-while (haystack[i])
-{
-while (needle[j] && (haystack[i] == needle[0]))
-{
-if (haystack[i + j] == needle[j])
-j++;
-else
-break;
-}
-if (needle[j])
-{
-i++;
-j = 0;
-}
-else
-return (haystack + i);
-}
-return (0);
+return (strstr(haystack, needle));
 }
