beac65731205e92ac25877bc9a9bd3400a7254c0
Winstone-owen <maywakaowen@gmail.com>
2024-07-08 09:50:16 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print the size of various types
+ *
+ * Description: prints the size of various types on the computer it is compiled
+ * and run on
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(STDERR_FILENO, msg, 42);
+	return (1);
+}
