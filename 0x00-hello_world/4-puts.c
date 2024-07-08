528e8e2fde1b374fa377b8445e96272bd2499a82
Winstone-owen <maywakaowen@gmail.com>
2024-07-08 09:24:04 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - the entry point of c program
+*
+* Return: 0 to end the program
+*/
+int main(void)
+{
+        /* Initializing strings */
+        char value[] = "\"Programming is like building a multilingual puzzle";
+        /**
+        * writing to stdout
+        */
+        puts(value);
+        return (0);
+}
