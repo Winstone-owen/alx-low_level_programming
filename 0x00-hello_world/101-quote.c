73d29a184695676f6312979f90f7025dfce220d0
Winstone-owen <maywakaowen@gmail.com>
2024-07-08 09:58:32 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point of the program
  *
- * Description: Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * Description: Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19,
  * followed by a new line, to the standard error.
  *
  * Return: Always 1 (Failure)
@@ -11,6 +11,7 @@
 int main(void)
 {
 	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	write(STDERR_FILENO, msg, sizeof(msg) - 1);
 	return (1);
 }
