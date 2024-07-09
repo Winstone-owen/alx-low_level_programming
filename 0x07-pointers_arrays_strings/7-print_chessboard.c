e2dbe44c002054bbc1069adf0ee6d1faabc4d6aa
Winstone-owen <maywakaowen@gmail.com>
2024-07-08 23:59:26 -0700

    print chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,44 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * print_chessboard - Prints a 2D array representing a chessboard.
- *
- * @a: The 2D array (8x8) to be printed.
- *
- * Return: No return value.
+ * print_chessboard - Prints 2D array
+ * @a: 2d array
+ * Return: No value
  */
 void print_chessboard(char (*a)[8])
 {
-	int i, j;
+        int i, j;
 
-	for (i = 0; i < 8; ++i)
-	{
-		for (j = 0; j < 8; ++j)
-		{
-			printf("%c ", a[i][j]);
-		}
-		printf("\n");
-	}
-}
-
-/**
- * main - Entry point.
- *
- * Return: Always 0 (Success).
- */
-int main(void)
-{
-	char chessboard[8][8] = {
-		{'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
-		{'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-		{'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-		{'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'}
-	};
-
-	print_chessboard(chessboard);
-	return (0);
+        for (i = 0; i < 8; i++)
+        {
+                for (j = 0; j < 8; j++)
+                {
+                        _putchar(a[i][j]);
+                }
+                _putchar('\n');
+        }
 }
