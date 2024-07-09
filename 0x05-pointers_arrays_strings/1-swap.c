2189c795af335bd1d25c028c40a4ec0226b90ecc
Winstone-owen <maywakaowen@gmail.com>
2024-07-09 08:09:36 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,10 @@
+#include"main.h"
+
+/**
+*swap_int-swaps the values of two integers.
+*@a:first integer.
+*@b:second integer.
+*Return:noreturn.
+*/
+void swap_int(int*a,int*b){intc=*a;*a=*b;*b=c;}
+
