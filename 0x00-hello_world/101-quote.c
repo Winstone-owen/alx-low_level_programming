5f074120b34f3cafa739e9c7b35bfb499fe34baa
Winstone-owen <maywakaowen@gmail.com>
2024-07-09 02:05:15 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,14 @@
 #include <unistd.h>
 
 /**
- * main - Entry point of the program
+ * main - Entry point
  *
- * Description: Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19,
- * followed by a new line, to the standard error.
- *
- * Return: Always 1 (Failure)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(STDERR_FILENO, msg, sizeof(msg) - 1);
+	write(2, str1, 59);
 	return (1);
 }
