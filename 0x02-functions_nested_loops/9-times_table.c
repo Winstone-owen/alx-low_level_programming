75720206f0fb050a60fcd04c861b4592bb5425dc
Winstone-owen <maywakaowen@gmail.com>
2024-07-09 03:45:32 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,29 +7,29 @@
  */
 void times_table(void)
 {
-	int a, b, op;
+    int a, b, op;
 
-	for (a = 0; a <= 9; a++)
-	{
-		_putchar('0'); // Print initial '0' for the first column
-		for (b = 1; b <= 9; b++)
-		{
-			op = a * b;
-			_putchar(',');
-			_putchar(' ');
+    for (a = 0; a <= 9; a++)
+    {
+        _putchar('0'); // Print initial '0' for the first column
+        for (b = 1; b <= 9; b++)
+        {
+            op = a * b;
+            _putchar(',');
+            _putchar(' ');
 
-			// Print the result with correct formatting
-			if (op < 10)
-			{
-				_putchar(' ');
-				_putchar(op + '0');
-			}
-			else
-			{
-				_putchar((op / 10) + '0');
-				_putchar((op % 10) + '0');
-			}
-		}
-		_putchar('\n');
-	}
+            // Print the result with correct formatting
+            if (op < 10)
+            {
+                _putchar(' ');
+                _putchar(op + '0');
+            }
+            else
+            {
+                _putchar((op / 10) + '0');
+                _putchar((op % 10) + '0');
+            }
+        }
+        _putchar('\n');
+    }
 }
