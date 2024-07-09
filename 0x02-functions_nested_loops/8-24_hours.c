b3709cc6131dd9be7b0cb2801ea19b3ce382c2e3
Winstone-owen <maywakaowen@gmail.com>
2024-07-09 03:57:00 -0700

    times table

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,44 +9,39 @@
  */
 void jack_bauer(void)
 {
-	int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
+        int hours_tens, hours_ones, minutes_tens, minutes_ones;
 
-	hours_max = '2'; /* Start with hours from '00' to '23' */
-	hours_tens = '0'; /* Initialize tens digit of hours */
+        hours_tens = '0'; /* Initialize tens digit of hours */
 
-	while (hours_tens <= '2')
-	{
-		if (hours_tens == '2')
-		{
-			hours_max = '3'; /* Change maximum hours to '03' to '23' */
-		}
-		hours_ones = '0'; /* Initialize ones digit of hours */
+        while (hours_tens <= '2')
+        {
+                hours_ones = '0'; /* Initialize ones digit of hours */
 
-		while (hours_ones < hours_max)
-		{
-			minutes_tens = '0'; /* Initialize tens digit of minutes */
+                while (hours_ones <= '9')
+                {
+                        minutes_tens = '0'; /* Initialize tens digit of minutes */
 
-			while (minutes_tens < '6')
-			{
-				minutes_ones = '0'; /* Initialize ones digit of minutes */
+                        while (minutes_tens <= '5')
+                        {
+                                minutes_ones = '0'; /* Initialize ones digit of minutes */
 
-				while (minutes_ones < '9') /* Changed '10' to '9' to avoid multi-character constant */
-				{
-					_putchar(hours_tens);
-					_putchar(hours_ones);
-					_putchar(':');
-					_putchar(minutes_tens);
-					_putchar(minutes_ones);
-					_putchar('\n');
-					minutes_ones++;
-				}
-				minutes_ones = '0'; /* Reset ones digit for next tens digit of minutes */
-				minutes_tens++;
-			}
-			minutes_tens = '0'; /* Reset tens digit for next hour */
-			hours_ones++;
-		}
-		hours_ones = '0'; /* Reset ones digit for next tens digit of hours */
-		hours_tens++;
-	}
+                                while (minutes_ones <= '9')
+                                {
+                                        _putchar(hours_tens);
+                                        _putchar(hours_ones);
+                                        _putchar(':');
+                                        _putchar(minutes_tens);
+                                        _putchar(minutes_ones);
+                                        _putchar('\n');
+                                        minutes_ones++;
+                                }
+                                minutes_tens++;
+                                minutes_ones = '0'; /* Reset ones digit for next tens digit of minutes */
+                        }
+                        hours_ones++;
+                        minutes_tens = '0'; /* Reset tens digit for next hour */
+                }
+                hours_tens++;
+                hours_ones = '0'; /* Reset ones digit for next tens digit of hours */
+        }
 }
