87f181068c13f2529c303e8542e5b67147529931
Winstone-owen <maywakaowen@gmail.com>
2024-07-15 07:17:16 -0700

    wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,20 +9,17 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-if (*s1 == '\0' && *s2 == '\0')
-return (1); // Both strings have ended simultaneously
-
-if (*s1 == *s2)
-return (wildcmp(s1 + 1, s2 + 1)); // Characters match, move to next
-
-if (*s2 == '*')
-{
-if (*(s2 + 1) == '\0')
-return (1); // '*' is at the end of s2, matches any remaining characters
-if (*s1 == '\0')
-return (wildcmp(s1, s2 + 1)); // s1 is empty, skip the '*'
-return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)); // Try to match '*' with characters in s1
-}
-
-return (0); // Characters do not match and s2 does not contain '*'
+        if (*s1 == '\0' && *s2 == '\0')
+                return (1);
+        if (*s1 == *s2)
+                return (wildcmp(s1 + 1, s2 + 1));
+        if (*s2 == '*')
+        {
+                if (*(s2 + 1) == '\0')
+                        return (1);
+                if (*s1 == '\0')
+                        return (wildcmp(s1, s2 + 1));
+                return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+        }
+        return (0);
 }
